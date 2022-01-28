d9d2056355d9538892aaaa3c0f045ab1717c1d68
jinjinzara <rubby0922@gmail.com>
2022-01-28 15:17:21 +0900

    Create main.py

---
+++ analysis/main.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 14 17:06:04 2022
+
+@author: rubby
+"""
+
+import json
+import os
+import pandas as pd
+import nltk
+import random
+from nltk.tokenize import RegexpTokenizer
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+from sklearn.manifold import TSNE
+from nltk.corpus import stopwords
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+nct_dir = 'data/@NCTsmtown'
+itzy_dir= 'data/@ITZYofficial'
+enhypen_dir = 'data/@ENHYPEN_members'
+
+def to_df(filedir):
+    df = pd.DataFrame(columns=['id', 'following_account', 'text'])
+    for filename in os.listdir(filedir):
+        tweets = [json.loads(line) for line in open(os.path.join(filedir, filename), 'r', encoding='utf-8')]
+        t_text = ''
+        t_id = filename[2:-4]
+        for tweet in tweets:
+            try:
+                t_text += tweet['text'] + ' '
+            except KeyError:
+                print('No text in this tweet.')
+        new_data = {'id' : t_id, 'following_account' : filedir[5:],'text' : t_text}
+        df = df.append(new_data, ignore_index=True)
+    df['tokens'] = df['text'].apply(nltk_tokenizer)
+    return df
+
+def nltk_tokenizer(_wd):
+    custom_stopwords = open('analysis/stopwords.txt', 'r').read().split('\n')
+    tokens = RegexpTokenizer('[\w]+').tokenize(_wd.lower())
+    for token in tokens:
+        if token in stopwords.words('english') or token in custom_stopwords or len(token) < 2:
+            tokens.remove(token)
+    return tokens
+
+def tagging(df):
+    doc_df = df[['id', 'tokens']].values.tolist()
+    tagged_data = [TaggedDocument(words=_d, tags=[uid]) for uid, _d in doc_df]
+    return tagged_data
+
+nct_df, itzy_df, enhypen_df = to_df(nct_dir), to_df(itzy_dir), to_df(enhypen_dir)
+merged_df = pd.concat([nct_df, itzy_df, enhypen_df])
+
+nct_tagged, itzy_tagged, enhypen_tagged = tagging(nct_df), tagging(itzy_df), tagging(enhypen_df)
+merge_tagged = nct_tagged + itzy_tagged + enhypen_tagged
+
+model = Doc2Vec(alpha=0.025, min_alpha=0.0001, seed=100)
+model.build_vocab(merge_tagged)
+
+max_epoch = 10
+for epoch in range(max_epoch):
+    print('iteration {0}'.format(epoch))
+    model.train(merge_tagged, total_examples=model.corpus_count, epochs=model.epochs)
+    model.alpha -= 0.002
+    model.min_alpha = model.alpha
+
+nct_tagged_sample = [tagged.words for tagged in random.sample(nct_tagged, 100)]
+itzy_tagged_sample = [tagged.words for tagged in random.sample(itzy_tagged, 100)]
+enhypen_tagged_sample = [tagged.words for tagged in random.sample(enhypen_tagged, 100)]
+
+merge_tagged_sample = nct_tagged_sample + itzy_tagged_sample + enhypen_tagged_sample
+embedding_vectors = [model.infer_vector(text) for text in merge_tagged_sample]
+similarity_matrix = pd.DataFrame(cosine_similarity(embedding_vectors, embedding_vectors))
+
+embedding_vectors = [model.infer_vector(tagged.words) for tagged in merge_tagged]
+two_dim_vectors = TSNE(n_components=2).fit_transform(embedding_vectors)
+fig, ax = plt.subplots(figsize=(16,20))
+sns.scatterplot(two_dim_vectors[:,0], two_dim_vectors[:,1], hue=merged_df['following_account'].values)
